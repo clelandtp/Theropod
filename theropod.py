c8360c54a15c16ce54d696a7e857e4f6177f7083
clelandtp <33493182+clelandtp@users.noreply.github.com>
2025-07-22 14:19:30 -0400

    Update theropod.py
    Updated v1.0.1

--- theropod.py
+++ theropod.py
@@ -12,31 +12,17 @@ import glob
 import numpy as np
 import pandas as pd
 import sys
+import argparse
 import subprocess
 
 def kde_sklearn(x, x_grid, bandwidth=0.001, **kwargs):
-    """Kernel Density Estimation with Scikit-learn"""
     kde_skl = KernelDensity(bandwidth=bandwidth, **kwargs)
     kde_skl.fit(x[:, np.newaxis])
     # score_samples() returns the log-likelihood of the samples
     log_pdf = kde_skl.score_samples(x_grid[:, np.newaxis])
     return np.exp(log_pdf)
 
-def create_q(width=1500, height=500,
-            main_title='Protein Mass Spectrum'):
-    tooltips = [
-        ('Mass','@Mass{0.0000}'),
-        ('Density','@Density')
-        ]
-    q = figure(
-        width=width, height=height,
-        title = main_title,
-        tools = 'xwheel_zoom,xpan,box_zoom,undo,reset,save',
-        tooltips=tooltips,
-        output_backend= "canvas",
-        )
-    return q
-q = create_q()
+
 
 def create_r(width=1500, height=500,
             main_title='Protein Mass Spectrum'):
@@ -54,8 +40,24 @@ def create_r(width=1500, height=500,
     return r
 r = create_r()
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("filename", type=str, help="Add a filename")
+parser.add_argument("--charge", type=int, default = 60, help="Set the number of charges to evaluate; Default = 60")
+parser.add_argument("--neighbor", type=int, default = 10, help="Set the number of either charge or isotope neighbors; Default = 10")
+parser.add_argument("--tod", type=int, default = 300, help="Set the minimum ion life time; Default = 300")
+parser.add_argument("--rsquare", type=int, default = 0.99, help="Set the r-squared value; Default = 0.99")
+parser.add_argument("--slopecal", type=int, default = 2.292161, help="Set the KDE slope calibration value; Default = 2.292161")
+parser.add_argument("--minmass", type=int, default = 5000, help="Set the minimum mass for the final neutral mass spectrum; Default = 5000")
+parser.add_argument("--maxmass", type=int, default = 75000, help="Set the maximum mass for the final neutral mass spectrum; Default = 75000")
+parser.add_argument("--chargecorrecttype", type=int, default = 1, help="Set the charge correction type 1 = isotopes, 2 = charge neighbors; Default = 1")
+parser.add_argument("--spectrumtype", type=int, default = 1, help="Set the spectrum type 1 = kernal density estimation, 2 = mass histogram; Default = 1")
+
+
+args = parser.parse_args()
+
 path = os.getcwd()
-outputname = sys.argv[1]
+outputname = args.filename
 stori_files = glob.glob(os.path.join(path, "*_stori.pkl"))
 print(stori_files)
 os.mkdir('output')
@@ -64,10 +66,12 @@ os.mkdir('output')
 #print(storidfs)
 slopes = []
 
+##Merge and filter pickle files
+
 for f in stori_files:
     df = pd.read_pickle(f, compression = 'xz')
-    td = 300
-    rv = 0.99
+    td = args.tod
+    rv = args.rsquare
 
     df['SlopeComb2']=np.where((df['P Death1']-0>= td) & (df['SciLin R-squared1']>= rv), df['SciLin Slope1'], 
                                 np.where((df['P Death2']-df['P Death1']>= td) & (df['SciLin R-squared2']>= rv), df['SciLin Slope2'],
@@ -91,48 +95,9 @@ df = pd.concat(slopes)
 df = df.sort_values('m/z')
 df.to_csv("output/"+outputname+".csv",index=False)
 
-df = df[((df['m/z']<=637.29) | 
-         (df['m/z']>=637.3))] 
 
-df = df[((df['m/z']<=638.29) | 
-         (df['m/z']>=638.297))] 
 
-df = df[((df['m/z']<=650.29) | 
-         (df['m/z']>=638.297))] 
-
-df = df[((df['m/z']<=654.25) | 
-         (df['m/z']>=655.25))] 
-
-df = df[((df['m/z']<=1629) | 
-         (df['m/z']>=1632.7))] 
-
-#df = df[((df['m/z']<=1637) | 
- #        (df['m/z']>=1639.7))] 
-
-#df = df[((df['m/z']<=1642.5) | 
- #       (df['m/z']>=1645))] 
-
-df = df[((df['m/z']<=535.25) | 
-         (df['m/z']>=535.35))] 
-
-df = df[((df['m/z']<=663.4) | 
-         (df['m/z']>=663.46))] 
-
-df = df[((df['m/z']<=664.4) | 
-         (df['m/z']>=664.46))] 
-
-df = df[((df['m/z']<=616.1) | 
-         (df['m/z']>=616.2))] 
-
-df = df[((df['m/z']<=617.1) | 
-        (df['m/z']>=617.2))]
-
-#df = df[((df['m/z']<=1586.4) | 
- #        (df['m/z']>=1600))]
-
-df = df[((df['m/z']<=637.2) | 
-         (df['m/z']>=638.32))]
-		 
+## Perform charge calculation and refinement
 ppm = 1.5
 thresh = (ppm/1e6)*df['m/z']
 df['mzcluster'] = df['m/z'].diff().gt(thresh).cumsum().add(1)
@@ -154,14 +119,14 @@ mzcluslabelsmean = mzcluslabelsmean.rename(columns={'SlopeComb2': 'SlopeComb2mzc
 
 df = df.merge(mzcluslabelsmean, 'left')
 
-df['Chargemzclust'] = np.round(df['SlopeComb2mzc'] /2.292161, decimals = 0)
+df['Chargemzclust'] = np.round(df['SlopeComb2mzc'] /args.slopecal, decimals = 0)
 df['Massmzclust'] = df['m/z']*df['Chargemzclust'] - df['Chargemzclust'] * 1.0078250319
 
 mzclusterrange = df.groupby('mzcluster').agg(mzmin=('m/z', 'min'), mzmax=('m/z', 'max'))
 
-ztest = 100
-isotest = 10
-zneigh = 10
+ztest = args.charge
+isotest = args.neighbor
+zneigh = args.neighbor
 zchange = np.arange(-ztest,ztest+1,1)
 isofind = np.arange(-isotest,isotest+1,1)
 zneigh2 = np.arange(-zneigh,zneigh+1,1)
@@ -181,29 +146,40 @@ mzidx = pd.IntervalIndex.from_arrays(mzclusterrange['mzmin'],
                                  mzclusterrange['mzmax'], 
                                  closed="both")
 
-for z in zchange:
-    #for n in zneigh2:
-     #   print("z", z,"neighbor", n)
-      #  cmzm[z,n] = df['Chargemzclust']+z
-       # cmzmneigh[z,n] = np.where(cmzm[z,n]>=5, (df['m/z']*cmzm[z,n]+(1.0078250319*n))/(cmzm[z,n]+n), 0)
-        #neighpos[z,n] = mzidx.get_indexer(cmzmneigh[z,n])
-        #cmzmlabels[z,n] =  np.where(neighpos[z,n] != -1, 1.3, 0)
+if args.chargecorrecttype == 2:
+    for z in zchange:
+        for n in zneigh2:
+            print("z", z,"neighbor", n)
+            cmzm[z,n] = df['Chargemzclust']+z
+            cmzmneigh[z,n] = np.where(cmzm[z,n]>=5, (df['m/z']*cmzm[z,n]+(1.0078250319*n))/(cmzm[z,n]+n), 0)
+            neighpos[z,n] = mzidx.get_indexer(cmzmneigh[z,n])
+            cmzmlabels[z,n] =  np.where(neighpos[z,n] != -1, 1, 0)
 
-    for iso in isofind:
-        print("z", z, "isotopologue", iso)
-        cmzm2[z,iso] = df['Chargemzclust']+z
-        cmzmiso[z,iso] = np.where(cmzm2[z,iso]>=5, df['m/z'] + (1.0078250319*(iso/cmzm2[z,iso])), 0)
-        isopos[z,iso] = mzidx.get_indexer(cmzmiso[z,iso])
-        cmzmlabels2[z,iso] =  np.where(isopos[z,iso] != -1, 1, 0)
+elif args.chargecorrecttype == 1:
+    for z in zchange:
+        for iso in isofind:
+            print("z", z, "isotopologue", iso)
+            cmzm2[z,iso] = df['Chargemzclust']+z
+            cmzmiso[z,iso] = np.where(cmzm2[z,iso]>=5, df['m/z'] + (1.0078250319*(iso/cmzm2[z,iso])), 0)
+            isopos[z,iso] = mzidx.get_indexer(cmzmiso[z,iso])
+            cmzmlabels2[z,iso] =  np.where(isopos[z,iso] != -1, 1, 0)
 
-
-for z in zchange: 
-    #neighsum[z] = sum([cmzmlabels[i] for i in cmzmlabels.keys() if i[0]==z])
-    isosum[z] = sum([cmzmlabels2[i] for i in cmzmlabels2.keys() if i[0]==z])
+if args.chargecorrecttype == 2:
+    for z in zchange: 
+        neighsum[z] = sum([cmzmlabels[i] for i in cmzmlabels.keys() if i[0]==z])
+        
+elif args.chargecorrecttype == 1:
+    for z in zchange:
+        isosum[z] = sum([cmzmlabels2[i] for i in cmzmlabels2.keys() if i[0]==z])
     #totalsum= {k: neighsum.get(k, 0) + isosum.get(k, 0) for k in set(neighsum) | set(isosum)}
     
-dftotsum = pd.DataFrame(isosum)
-dftotsum = dftotsum.replace(0, 1.0)
+if args.chargecorrecttype == 2:    
+    dftotsum = pd.DataFrame(neighsum)
+
+elif args.chargecorrecttype == 1:
+    dftotsum = pd.DataFrame(isosum)
+
+#dftotsum = dftotsum.replace(0, 1.0)
 dftotsum2 = pd.merge(df[['m/z', 'Chargemzclust']], dftotsum, left_index=True, right_index=True)
 dftotsum2['cmzCheck'] = dftotsum2.loc[:, 0:].sum(axis = 1)
 dftotsum2['cmzChargeadjust'] = dftotsum2.iloc[:, 2:-1].idxmax(axis=1).astype('int64')
@@ -213,6 +189,7 @@ dftotsum2['cmzfinalcharge'] = dftotsum2['Chargemzclust']+dftotsum2['cmzChargeadj
 dftotsum2['mzcluster'] = df['mzcluster']
 df['cmzfinalcharge'] = dftotsum2['cmzfinalcharge']
 df['Massmzclust2'] = df['m/z']*df['cmzfinalcharge'] - df['cmzfinalcharge'] * 1.0078250319
+df['ioncount']=abs((df['Chargemzclust']/df['cmzfinalcharge'])).replace([np.inf, -np.inf], 1).apply(np.ceil).astype(int)
 df.to_csv("output/"+outputname+"_final.csv",index=False)
 
 mz = df['m/z'].to_numpy()
@@ -243,50 +220,130 @@ plt.figure()
 sns.histplot(data=df, x= 'cmzfinalcharge', y = 'Massmzclust2',fill=True,  binwidth = [1,1000], cbar = True)
 plt.savefig('output/finalchargevmass.png')
 
+plt.figure()
+y=sns.histplot(df['ioncount'], binwidth = 1, discrete = True)
+y.bar_label(y.containers[1])
+plt.savefig('output/ioncount.png')
 
-x = df['Massmzclust2'].to_numpy()
+
+
 #mczkde_range = np.arange(x.min(), x.max(),0.1)
-mczkde_range = np.arange(5000, 75000,0.05)
-pdf = kde_sklearn(x, mczkde_range, bandwidth=0.05, rtol=1E-4)
-df25 = pd.DataFrame({'Mass': mczkde_range, 'Density': pdf}, columns=['Mass', 'Density'])
-df25['Density'] = df25['Density']*100000000
-df25['relDensity'] = (df25['Density']/(df25['Density'].max()))*100
+if args.spectrumtype == 1:
+    def create_q(width=1500, height=500,
+                main_title='Protein Mass Spectrum'):
+        tooltips = [
+            ('Mass','@Mass{0.0000}'),
+            ('Density','@Density')
+            ]
+        q = figure(
+            width=width, height=height,
+            title = main_title,
+            tools = 'xwheel_zoom,xpan,box_zoom,undo,reset,save',
+            tooltips=tooltips,
+            output_backend= "canvas",
+            )
+        return q
+    q = create_q()
+    df2 = df.loc[df.index.repeat(df.ioncount)]
+    x = df2['Massmzclust2'].to_numpy()
+    mczkde_range = np.arange(args.minmass, args.maxmass,0.05)
+    pdf = kde_sklearn(x, mczkde_range, bandwidth=0.05, rtol=1E-4)
+    df25 = pd.DataFrame({'Mass': mczkde_range, 'Density': pdf}, columns=['Mass', 'Density'])
+    df25['Density'] = df25['Density']*100000000
+    df25['relDensity'] = (df25['Density']/(df25['Density'].max()))*100
+    output_file(filename="output/plotcanvasKDE.html", title="Mass spectrum")
+    cds = ColumnDataSource(data=df25)
+    q = create_q()
+    maxIntens = df25['relDensity'].max()
+    #Main line
+    q.line(
+        'Mass', 'relDensity',
+        source = cds,
+        color = 'black',# alpha = 0.8,
+        line_width = 2
+        )
 
-output_file(filename="output/plotcanvas1.html", title="Mass spectrum")
-cds = ColumnDataSource(data=df25)
+    #Format axis labels
+    def add_axis_labels(q):
+        q.xaxis.axis_label = 'Mass'
+        q.xaxis.axis_label_text_font_size = '10pt'
+        q.xaxis.major_label_text_font_size = '9pt'
 
-q = create_q()
-maxIntens = df25['relDensity'].max()
-#Main line
-q.line(
-    'Mass', 'relDensity',
-    source = cds,
-    color = 'black',# alpha = 0.8,
-    line_width = 2
-    )
+        q.yaxis.axis_label = 'Relative Density'
+        q.yaxis.axis_label_text_font_size = '10pt'
+        q.yaxis.major_label_text_font_size = '9pt'
+        q.yaxis.formatter = NumeralTickFormatter(format='0.')
+    add_axis_labels(q)
 
-#Format axis labels
-def add_axis_labels(q):
-    q.xaxis.axis_label = 'Mass'
-    q.xaxis.axis_label_text_font_size = '10pt'
-    q.xaxis.major_label_text_font_size = '9pt'
+    show(q)
+    save(q)
+    df26 = df25[['Mass', 'Density']].copy()
+    df27 = df25[['Mass', 'Density']].copy()
+    df26['Mass'] = df26['Mass']+1.0078250319
+    df26 = df26.rename(columns={'Mass': 'mz', 'Density': 'intensity'})
+    df27 = df27.rename(columns={'Mass': 'mz', 'Density': 'intensity'})
+    df26.to_csv("output/"+outputname+'-MH.csv', index = False)
+    df27.to_csv("output/"+outputname+'-M.csv', index = False)
+        
+elif args.spectrumtype == 2:
+    def create_q(width=1500, height=500,
+                main_title='Protein Mass Spectrum'):
+        tooltips = [
+            ('Mass','@Mass{0.0000}'),
+            ('Count','@Count')
+            ]
+        q = figure(
+            width=width, height=height,
+            title = main_title,
+            tools = 'xwheel_zoom,xpan,box_zoom,undo,reset,save',
+            tooltips=tooltips,
+            output_backend= "canvas",
+            )
+        return q
+    q = create_q()
+    x = df['Massmzclust2'].to_numpy()
+    mcz_range = np.arange(5000, 40000,0.05)
+    histo = np.histogram(x, mcz_range, weights = df['ioncount'])
+    histo2 = histo[0]
+    mcz_range2 = histo[1]
+    mcz_range2 = mcz_range[0:-1]
+    df25 = pd.DataFrame({'Mass': mcz_range2, 'Count': histo2}, columns=['Mass', 'Count'])
+    df25['relCount'] = (df25['Count']/(df25['Count'].max()))*100
+    output_file(filename="output/plotcanvasHistogram.html", title="Mass spectrum")
+    cds = ColumnDataSource(data=df25)
+    q = create_q()
+    maxIntens = df25['relCount'].max()
+    #Main line
+    q.line(
+        'Mass', 'relCount',
+        source = cds,
+        color = 'black',# alpha = 0.8,
+        line_width = 2
+        )
 
-    q.yaxis.axis_label = 'Relative Density'
-    q.yaxis.axis_label_text_font_size = '10pt'
-    q.yaxis.major_label_text_font_size = '9pt'
-    q.yaxis.formatter = NumeralTickFormatter(format='0.')
-add_axis_labels(q)
+    #Format axis labels
+    def add_axis_labels(q):
+        q.xaxis.axis_label = 'Mass'
+        q.xaxis.axis_label_text_font_size = '10pt'
+        q.xaxis.major_label_text_font_size = '9pt'
 
-show(q)
-save(q)
+        q.yaxis.axis_label = 'Relative Count'
+        q.yaxis.axis_label_text_font_size = '10pt'
+        q.yaxis.major_label_text_font_size = '9pt'
+        q.yaxis.formatter = NumeralTickFormatter(format='0.')
+    add_axis_labels(q)
 
-df26 = df25[['Mass', 'Density']].copy()
-df27 = df25[['Mass', 'Density']].copy()
-df26['Mass'] = df26['Mass']+1.0078250319
-df26 = df26.rename(columns={'Mass': 'mz', 'Density': 'intensity'})
-df27 = df27.rename(columns={'Mass': 'mz', 'Density': 'intensity'})
-df26.to_csv("output/"+outputname+'-MH.csv', index = False)
-df27.to_csv("output/"+outputname+'-M.csv', index = False)
+    show(q)
+    save(q)
+    df26 = df25[['Mass', 'Count']].copy()
+    df27 = df25[['Mass', 'Count']].copy()
+    df26['Mass'] = df26['Mass']+1.0078250319
+    df26 = df26.rename(columns={'Mass': 'mz', 'Count': 'intensity'})
+    df27 = df27.rename(columns={'Mass': 'mz', 'Count': 'intensity'})
+    df26.to_csv("output/"+outputname+'-MH.csv', index = False)
+    df27.to_csv("output/"+outputname+'-M.csv', index = False)
+
+
 
 script_path = os.path.join(os.path.dirname(__file__), "theropod_mzML.R")
 
